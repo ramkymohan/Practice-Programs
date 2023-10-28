2e7421aeb16cbd45cd17a38877476e31e1718529
Ramakrishna_Doddi <ramakrishna+_doddi@epam.com>
2023-10-28 23:16:43 +0530

    employee file

--- src/EmployeeManagement.java
+++ src/EmployeeManagement.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class EmployeeManagement {
+import java.util.Date;
+
+class Employee {
+    private String empId;
+    private String empName;
+    private Date dob;
+    private Date doj;
+
+    // Constructors, getters, and setters
+
+    // Other employee-related methods
+}
+
+class EmployeeAttendance {
+    private String empId;
+    private String empName;
+    private int daysPresent;
+
+    // Constructors, getters, and setters
+
+    // Other attendance-related methods
+}
+
+class PayDetails {
+    private String empId;
+    private String empName;
+    private double empGrossRemuneration;
+    private int daysPresent;
+
+    // Constructors, getters, and setters
+
+    // Other pay-related methods
+}
+
+class EmployeeManagementSystem {
+    // Database connection and operations
+    // - Implement methods to insert, update, and retrieve employee data
+    // - Implement methods to insert and retrieve attendance data
+    // - Implement methods to calculate pay details
+
+    public void insertEmployee(Employee employee) {
+        // Code to insert employee data into the database
+    }
+
+    public void updateEmployee(Employee employee) {
+        // Code to update employee data in the database
+    }
+
+    public Employee getEmployeeById(String empId) {
+        // Code to retrieve an employee by empId from the database
+        return null;
+    }
+
+    public void insertEmployeeAttendance(EmployeeAttendance attendance) {
+        // Code to insert attendance data into the database
+    }
+
+    public void generatePayslip(PayDetails payDetails) {
+        // Code to calculate pay details and generate a payslip
+    }
+
+    // Other methods for database operations
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        // Initialize the EmployeeManagementSystem and connect to the database
+
+        EmployeeManagementSystem ems = new EmployeeManagementSystem();
+
+        // Create an Employee object and insert it into the database
+        Employee employee = new Employee();
+        // Set employee details
+        ems.insertEmployee(employee);
+
+        // Retrieve an employee by ID and update their information
+        Employee retrievedEmployee = ems.getEmployeeById("A123");
+        // Update employee details
+        ems.updateEmployee(retrievedEmployee);
+
+        // Create EmployeeAttendance objects and insert them into the database
+
+        // Calculate pay details and generate payslips
+
+        // Display the final list of employees and their pay details
+    }
 }
